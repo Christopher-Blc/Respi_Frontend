7876c8978e9cbc9bc2bc77cd304532e7d0e26247
Christopher Bolocan <christopherbolocan.pr@clustag.com>
2026-03-30 08:51:31 +0200

    Actualizaciones para que no pete en web (usando localstorage en caso de web) y refactorizacion de authcontext

--- src/services/authStorage.ts
+++ src/services/authStorage.ts
@@ -1,15 +1,32 @@
 import * as SecureStore from 'expo-secure-store';
+import { Platform } from 'react-native';
 
 const TOKEN_KEY = 'user_auth_token';
 
 export const saveToken = async (token: string) => {
-  await SecureStore.setItemAsync(TOKEN_KEY, token);
+
+  if (Platform.OS === 'web') {
+    return localStorage.setItem(TOKEN_KEY, token);
+  }else{
+    await SecureStore.setItemAsync(TOKEN_KEY, token);
+  }
 };
 
 export const getToken = async () => {
-  return await SecureStore.getItemAsync(TOKEN_KEY);
+
+  if (Platform.OS === 'web') {
+    return localStorage.getItem(TOKEN_KEY);
+  }else{
+    return await SecureStore.getItemAsync(TOKEN_KEY);
+  }
+
 };
 
 export const deleteToken = async () => {
-  await SecureStore.deleteItemAsync(TOKEN_KEY);
+
+  if(Platform.OS === 'web'){
+    return localStorage.removeItem(TOKEN_KEY);
+  }else{
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+  }
 };
