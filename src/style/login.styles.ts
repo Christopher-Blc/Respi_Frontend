c9395ffa6500b47ea423820cfca6482a2250b527
Christopher Bolocan <christopherbolocan.pr@clustag.com>
2026-03-30 17:57:40 +0200

    styles de login y register externo

---
+++ src/style/login.styles.ts
@@ -0,0 +1,77 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  test404Button: {
+    position: 'absolute',
+    bottom: 40,
+    right: 80,
+    zIndex: 10,
+    backgroundColor:"#5100ff"
+  },
+  darkModeButton: {
+    position: 'absolute',
+    bottom: 40,
+    right: 20,
+    zIndex: 10,
+    backgroundColor:"#5100ff"
+  },
+  background: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+  },
+  glass: {
+    width: '85%',
+    maxWidth: 340,
+    padding: 25,
+    borderRadius: 30, 
+    borderWidth: 0.8,
+    borderColor: "#ccc",
+    //borderWidth: 0.5,
+    //borderColor: '#616161',
+    //backgroundColor: 'rgba(255, 170, 0, 0.12)',
+    overflow: 'hidden',
+    alignItems: 'center', 
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: '700',
+    marginBottom: 20,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+    fontWeight: '500',
+    fontSize: 16,
+    color: '#444', // Un pelín más oscuro para contraste
+    textAlign: 'center',
+    lineHeight: 22,
+    // Sombra de texto (funciona en iOS, Android y Web)
+    textShadowColor: 'rgba(255, 255, 255, 0.6)',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 5,
+  },
+  input: {
+    width: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.1)', 
+    borderRadius: 12,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.12)',
+    color: '#000000', 
+  },
+  error: {
+    color: '#D32F2F',
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;              
